c71d9360dfd7eafc38f83f635b0736b83bcb95c9
dan <dany.condurari@gmail.com>
2023-06-08 16:51:06 +0300

    fixes

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -7,7 +7,7 @@ const LoadingPage = () => {
       className="flex items-center justify-center max-w-7xl mx-auto h-[47vh]  "
     >
       <div className="w-5 h-5">
-        <h1>Loading...</h1>
+        <h1 className="dark:text-white">Loading...</h1>
       </div>
     </div>
   );
